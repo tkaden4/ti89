865ca5aefc71c4a738536a4532387b2fa9e1b213
Kaden Thomas <thomas.kaden4@outlook.com>
2024-04-20 23:21:43 -0700

    update makefile to use bin directory

--- programs/cheat.c
+++ programs/cheat.c
@@ -6,6 +6,26 @@
 
 int _ti89;
 
+/*
+Cheating Application
+
+Connect: <0x00> <SSID> <PASS>
+
+User Commands: GetAnswer, SetAnswer
+
+GetAnswer: <0x01> <path>
+
+SetAnswer: <0x02> <path> <answer>
+
+<path>: <byte[]>
+
+<answer>: <type> <value>
+        : <0x00> <byte>   // single choice
+        : <0x01> <byte[]> // multiple choices
+        : <0x02> <byte[]> // long answer
+
+*/
+
 int __main()
 {
   link_init();
